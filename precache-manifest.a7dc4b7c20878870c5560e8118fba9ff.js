c9908667499e3073372d1b2cd43b8d229423401b
Ross Ferreira <ross.p.ferreira@gmail.com>
2020-04-23 12:53:57 +0100

    Updates

---
+++ precache-manifest.a7dc4b7c20878870c5560e8118fba9ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd78fdfeeca1274651c9de9e386baf8",
+    "url": "/pairs_app/index.html"
+  },
+  {
+    "revision": "eb022667759109524d05",
+    "url": "/pairs_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "659b64cc68079e352a8b",
+    "url": "/pairs_app/static/js/2.bbff0ff4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pairs_app/static/js/2.bbff0ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb022667759109524d05",
+    "url": "/pairs_app/static/js/main.ea3b9ca7.chunk.js"
+  },
+  {
+    "revision": "9530a5b742addda47086",
+    "url": "/pairs_app/static/js/runtime-main.c227c15a.js"
+  }
+]);
