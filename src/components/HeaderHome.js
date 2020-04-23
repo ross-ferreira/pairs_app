aa1846ca4eb63ef5591143a0586c21d177a63aea
Ross Ferreira <ross.p.ferreira@gmail.com>
2020-04-23 14:29:41 +0100

    title changes

--- src/components/HeaderHome.js
+++ src/components/HeaderHome.js
@@ -10,8 +10,8 @@ class HeaderHome extends Component {
                 <header className="main-header">
 
                     {/* <h4 className="main-title">RPF developments presents...</h4> */}
-                    <h1 class="main-title">Pear Challenge</h1>
-                    <h2 class="sub-main-title">Lets get Pearing</h2>
+                    <h1 class="main-title">Pair Challenge</h1>
+                    <h2 class="sub-main-title">Lets get Pairing</h2>
 
                 </header>
             </>
